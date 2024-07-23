f6ee53fb4246465eea10e59815a2539787eb43b1
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-07-23 10:45:06 +0000

    Adds test for reverse function

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ const addNums = (a, b) => {
   return a + b;
 };
 
+const reverse = (string) => {
+  return string.split("").reverse().join("");
+};
+
 module.exports = {
   greetUser,
   addNums,
+  reverse,
 };
--- script.test.js
+++ script.test.js
@@ -1,5 +1,9 @@
-const { greetUser, addNums } = require("./script");
+const { greetUser, addNums, reverse } = require("./script");
 
 test("takes user name and prints it", () => {
   expect(greetUser("Sam")).toBe("Hello Sam");
+});
+
+test("takes a string and reverses it", () => {
+  expect(reverse("tame")).toBe("emat");
 });
